d0d19856532d816960f7bfcfe51f6efcfb743ab0
kishan1988-hub <69946669+kishan1988-hub@users.noreply.github.com>
2022-07-07 17:15:33 +0530

    Update count-parameter.tf

--- 02 - Output-Attribute-Variables/count-parameter.tf
+++ 02 - Output-Attribute-Variables/count-parameter.tf
@@ -8,3 +8,20 @@ resource "aws_iam_user" "lb_user" {
     count = 3
     path = "/system/"
 }
+
+
+resource "aws_instance" "Dev" {
+  ami             = "ami-0a9d27a9f4f5c0efc"
+  instance_type   = "t2.micro"
+  key_name        = "dropmailtokishan" # this is the pem file name associated with the server
+  security_groups = ["launch-wizard-1"]
+  count = 3
+}
+
+resource "aws_instance" "Dev" {
+  ami             = "ami-0a9d27a9f4f5c0efc"
+  instance_type   = "t2.micro"
+  key_name        = "dropmailtokishan" # this is the pem file name associated with the server
+  security_groups = ["launch-wizard-1"]
+  count = var.istest == true ? 3 : 0
+}
